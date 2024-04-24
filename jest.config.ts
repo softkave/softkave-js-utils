ae0fbd2118cc6ea5f9d0d4ecaa3d92abb4970089
Abayomi Akintomide <ywordk@gmail.com>
2024-04-24 17:23:23 -0400

    Jest updates

--- jest.config.ts
+++ jest.config.ts
@@ -6,12 +6,6 @@ const config: JestConfigWithTsJest = {
   testPathIgnorePatterns: ['/jest/', '/build/', '/testUtils.ts'],
   testTimeout: 300000, // 300 seconds
   transformIgnorePatterns: ['/node_modules/(?!(nanoid)/)'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
-  },
 };
 
 module.exports = config;
